d3d4d13f0b51f309d503e935cdd1a2a413dc3385
Artyom Danilov <defytheflow@gmail.com>
2022-12-12 17:30:40 +0300

    fix: solve 5 with parsing stacks from the input.txt

--- 2022/5/b.py
+++ 2022/5/b.py
@@ -1,16 +1,17 @@
-stacks = [
-    ["H", "R", "B", "D", "Z", "F", "L", "S"],
-    ["T", "B", "M", "Z", "R"],
-    ["Z", "L", "C", "H", "N", "S"],
-    ["S", "C", "F", "J"],
-    ["P", "G", "H", "W", "R", "Z", "B"],
-    ["V", "J", "Z", "G", "D", "N", "M", "T"],
-    ["G", "L", "N", "W", "F", "S", "P", "Q"],
-    ["M", "Z", "R"],
-    ["M", "C", "L", "G", "V", "R", "T"],
-]
-
 with open("./input.txt") as f:
+    stack_lines = []
+    for line in f:
+        if line == "\n":
+            break
+        stack_lines.append(line)
+
+    stack_lines = stack_lines[-2::-1]
+    stacks = []
+    for i in range((len(stack_lines[0])) // 4):
+        start = i * 4 + 1
+        stack = [line[start : start + 1] for line in stack_lines]
+        stacks.append([crate for crate in stack if crate != " "])
+
     for line in f:
         words = line.strip().split(" ")
         count = int(words[1])

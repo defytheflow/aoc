aeb0b132239cca96060f16d8a4e8768ad2e8e590
Artyom Danilov <defytheflow@gmail.com>
2022-12-15 12:38:45 +0300

    fix: solve day 6 more efficiently memory wise

--- 2022/6/a.py
+++ 2022/6/a.py
@@ -1,12 +1,8 @@
 from pathlib import Path
 
-with open(Path(__file__).parent.joinpath('input.txt')) as f:
-    n_unique_characters = 4
+with open(Path(__file__).parent.joinpath("input.txt")) as f:
     buffer = f.read()
-    window = list(buffer[:n_unique_characters])
-    for i, char in enumerate(buffer[n_unique_characters:]):
-        if len(window) == len(set(window)):
-            print(i + n_unique_characters)
+    for i in range(4, len(buffer)):
+        if len(set(buffer[i - 4 : i])) == 4:
+            print(i)
             break
-        window.pop(0)
-        window.append(char)
--- 2022/6/b.py
+++ 2022/6/b.py
@@ -1,12 +1,8 @@
 from pathlib import Path
 
-with open(Path(__file__).parent.joinpath('input.txt')) as f:
-    n_unique_characters = 14
+with open(Path(__file__).parent.joinpath("input.txt")) as f:
     buffer = f.read()
-    window = list(buffer[:n_unique_characters])
-    for i, char in enumerate(buffer[n_unique_characters:]):
-        if len(window) == len(set(window)):
-            print(i + n_unique_characters)
+    for i in range(14, len(buffer)):
+        if len(set(buffer[i - 14 : i])) == 14:
+            print(i)
             break
-        window.pop(0)
-        window.append(char)

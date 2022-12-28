7e95116ffa258af0b85ba39d7002174565f0927e
Artyom Danilov <defytheflow@gmail.com>
2022-12-28 12:36:43 +0300

    feat: solve day 15 part 2

--- 2022/15/a.py
+++ 2022/15/a.py
@@ -6,18 +6,14 @@ with open(Path(__file__).parent.joinpath("input.txt")) as f:
 
     for line in f:
         line = line.rstrip("\n")
-        sensor_x, sensor_y = [
+        sx, sy = [
             int(s[2:]) for s in line[line.index("x") : line.index(":")].split(", ")
         ]
-        beacon_x, beacon_y = [int(s[2:]) for s in line[line.rindex("x") :].split(", ")]
+        bx, by = [int(s[2:]) for s in line[line.rindex("x") :].split(", ")]
+        dist = abs(sx - bx) + abs(sy - by)
 
-        distance = abs(sensor_x - beacon_x) + abs(sensor_y - beacon_y)
-        start_x = sensor_x - distance
-        end_x = sensor_x + distance
-
-        for x in range(start_x, end_x + 1):
-            dist = abs(sensor_x - x) + abs(sensor_y - y)
-            if dist <= distance:
+        for x in range(sx - dist, sx + dist + 1):
+            if abs(sx - x) + abs(sy - y) <= dist:
                 xs.add(x)
 
     n_positions = len(xs) - 1  # minus the beacon position

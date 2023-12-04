4fb143cf14eaed83d7e1a7f37ca567a4c5bae9a9
Artyom Danilov <defytheflow@gmail.com>
2023-12-05 02:08:54 +0300

    feat(2023): solve first part of day 4

---
+++ 2023/04/solve.ts
@@ -0,0 +1,42 @@
+import fs from "node:fs";
+
+type Input = [number[], number[]][];
+
+main();
+
+function main() {
+  const input = parseInput("input.txt");
+
+  const resultOne = solveOne(input);
+  console.log(resultOne);
+  console.assert(resultOne == 18_619);
+
+  const resultTwo = solveTwo(input);
+  console.log(resultTwo);
+  // console.assert(resultTwo == );
+}
+
+function solveOne(input: Input): number {
+  return input
+    .map(([winning, mine]) => mine.filter(n => winning.includes(n)))
+    .reduce((total, winning) => total + Math.floor(2 ** (winning.length - 1)), 0);
+}
+
+function solveTwo(input: Input): number {}
+
+function parseInput(filename: string): Input {
+  return fs
+    .readFileSync(filename)
+    .toString()
+    .split(/\n|\r\n/)
+    .map(
+      line =>
+        line
+          .slice(line.indexOf(":") + 2)
+          .split(" | ")
+          .map(part => part.split(" ").filter(Boolean).map(Number)) as [
+          number[],
+          number[]
+        ]
+    );
+}

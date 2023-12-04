41613c651e3199094a60f86902a7381449a4a8b3
Artyom Danilov <defytheflow@gmail.com>
2023-12-05 01:36:09 +0300

    feat(2023): solve second part of day 3

--- 2023/03/solve.ts
+++ 2023/03/solve.ts
@@ -23,29 +23,24 @@ function main() {
 
   const resultTwo = solveTwo(input);
   console.log(resultTwo);
-  // console.assert(resultTwo == );
+  console.assert(resultTwo == 84_495_585);
 }
 
 function solveOne(input: Input): number {
   const { grid, numbers } = input;
 
   return numbers
-    .filter(number => {
-      return number.some(entry => {
-        return neighbors(entry.position).some(
-          char => char != undefined && char != "." && !/\d/.test(char)
-        );
-      });
-    })
-    .map(number =>
-      parseInt(
-        number.reduce((s, entry) => s + entry.digit, ""),
-        10
+    .filter(number =>
+      number.some(entry =>
+        neighbors(entry.position).some(char => char != "." && !/\d/.test(char))
       )
     )
+    .map(toNumber)
     .reduce((a, b) => a + b, 0);
 
-  function neighbors(position: Position): (string | undefined)[] {
+  // --------------------------------------------------------------------------------
+
+  function neighbors(position: Position): string[] {
     return [
       grid[position.y][position.x - 1],
       grid[position.y][position.x + 1],
@@ -55,15 +50,77 @@ function solveOne(input: Input): number {
       grid[position.y + 1]?.[position.x + 1],
       grid[position.y + 1]?.[position.x - 1],
       grid[position.y - 1]?.[position.x + 1],
-    ];
+    ].filter(Boolean);
   }
 }
 
-function solveTwo(input: Input): number {}
+function solveTwo(input: Input): number {
+  const { grid, numbers } = input;
+  const gearPositions = collectGearPositions();
+
+  let sum = 0;
+
+  for (const gearPosition of gearPositions) {
+    const digitPositions: Position[] = [];
+
+    for (const [y, x] of [
+      [gearPosition.y, gearPosition.x - 1],
+      [gearPosition.y, gearPosition.x + 1],
+      [gearPosition.y - 1, gearPosition.x],
+      [gearPosition.y + 1, gearPosition.x],
+      [gearPosition.y - 1, gearPosition.x - 1],
+      [gearPosition.y + 1, gearPosition.x + 1],
+      [gearPosition.y + 1, gearPosition.x - 1],
+      [gearPosition.y - 1, gearPosition.x + 1],
+    ]) {
+      if (/\d/.test(grid[y]?.[x])) {
+        digitPositions.push({ x, y });
+      }
+    }
+
+    const partNumbers: number[] = [];
+
+    for (const number of numbers) {
+      if (
+        number.some(entry =>
+          digitPositions.some(p => entry.position.x == p.x && entry.position.y == p.y)
+        )
+      ) {
+        partNumbers.push(toNumber(number));
+      }
+    }
+
+    if (partNumbers.length == 2) {
+      sum += partNumbers[0] * partNumbers[1];
+    }
+  }
+
+  return sum;
+
+  // --------------------------------------------------------------------------------
+
+  function collectGearPositions(): Position[] {
+    return grid.flatMap(
+      (row, y) =>
+        row
+          .map((char, x) => (char == "*" ? { x, y } : null))
+          .filter(Boolean) as Position[]
+    );
+  }
+}
+
+function toNumber(theNumber: TNumber): number {
+  return parseInt(
+    theNumber.map(entry => entry.digit).reduce((a, b) => a + b, ""),
+    10
+  );
+}
 
 function parseInput(filename: string): Input {
   const content = fs.readFileSync(filename).toString();
   return { grid: parseGrid(content), numbers: parseNumbers(content) };
+
+  // --------------------------------------------------------------------------------
 
   function parseGrid(content: string): Input["grid"] {
     return content.split(/\n|\r\n/).map(line => [...line]);
@@ -84,14 +141,3 @@ function parseInput(filename: string): Input {
     });
   }
 }
-
-/*
-[
-  [
-    { digit: 4, position: { x: 0, y: 0 } },
-    { digit: 6, position: { x: 1, y: 0 } },
-    { digit: 7, position: { x: 2, y: 0 } },
-  ],
-  ...
-]
-*/

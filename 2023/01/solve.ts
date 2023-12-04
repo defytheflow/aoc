9e9a801e9d34ca3b5b10f92a90f9f5c39dd5713e
Artyom Danilov <defytheflow@gmail.com>
2023-12-04 11:15:05 +0300

    feat(2023): solve day 1

---
+++ 2023/01/solve.ts
@@ -0,0 +1,74 @@
+import fs from "node:fs";
+
+type Input = string[];
+
+main();
+
+function main() {
+  const input = parseInput("input.txt");
+
+  const resultOne = solveOne(input);
+  console.log(resultOne);
+  console.assert(resultOne == 53_334);
+
+  const resultTwo = solveTwo(input);
+  console.log(resultTwo);
+  console.assert(resultTwo == 52_834);
+}
+
+function solveOne(input: Input): number {
+  return input
+    .map(line => line.match(/\d/g))
+    .map(numbers => numbers.at(0) + numbers.at(-1))
+    .map(Number)
+    .reduce((a, b) => a + b, 0);
+}
+
+function solveTwo(input: Input): number {
+  const numbersMap = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  return solveOne(input.map(parseDigits));
+
+  // --------------------------------------------------------------------------------
+
+  function parseDigits(line: string) {
+    let parsed = "";
+    let curr = "";
+
+    for (const char of line) {
+      if (/\d/.test(char)) {
+        parsed += char;
+        curr = "";
+      } else {
+        curr += char;
+        for (const [key, value] of Object.entries(numbersMap)) {
+          if (curr.match(key)) {
+            parsed += value;
+            curr = "";
+            break;
+          }
+        }
+      }
+    }
+
+    return parsed;
+  }
+}
+
+function parseInput(filename: string): Input {
+  return fs
+    .readFileSync(filename)
+    .toString()
+    .split(/\n|\r\n/)
+    .filter(Boolean);
+}

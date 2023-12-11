485f2da38629efea4038b308e38859785c011614
Artyom Danilov <defytheflow@gmail.com>
2023-12-12 00:24:26 +0300

    feat(2023): solve second part of day 8

--- 2023/08/solve.ts
+++ 2023/08/solve.ts
@@ -8,7 +8,7 @@ type Input = {
 main();
 
 function main() {
-  const input = parseInput("input.txt");
+  const input = parseInput("input2.txt");
 
   const resultOne = solveOne(input);
   console.log(resultOne);
@@ -16,7 +16,7 @@ function main() {
 
   const resultTwo = solveTwo(input);
   console.log(resultTwo);
-  console.assert(resultTwo == Infinity);
+  console.assert(resultTwo == 9_606_140_307_013);
 }
 
 function solveOne(input: Input): number {
@@ -41,7 +41,53 @@ function solveOne(input: Input): number {
 }
 
 function solveTwo(input: Input): number {
-  return Infinity;
+  const { instruction, network } = input;
+
+  const currentNodes = Object.keys(network).filter(node => node.endsWith("A"));
+  const steps = currentNodes.map(node => countSteps(node, network, instruction));
+
+  return findLCM(steps);
+
+  // --------------------------------------------------------------------------------
+
+  function countSteps(
+    node: string,
+    network: Input["network"],
+    instruction: Input["instruction"]
+  ): number {
+    let currentNode = node;
+    let currentInstructionIndex = 0;
+    let steps = 0;
+
+    while (!currentNode.endsWith("Z")) {
+      steps += 1;
+      const currentInstruction = instruction[currentInstructionIndex];
+
+      if (currentInstruction == "L") {
+        currentNode = network[currentNode][0];
+      } else if (currentInstruction == "R") {
+        currentNode = network[currentNode][1];
+      }
+
+      currentInstructionIndex = (currentInstructionIndex + 1) % instruction.length;
+    }
+
+    return steps;
+  }
+
+  function findLCM(numbers: number[]): number {
+    // Greatest Common Divisor
+    function gcd(a: number, b: number): number {
+      return b == 0 ? a : gcd(b, a % b);
+    }
+
+    // Least Common Multiple
+    function lcm(a: number, b: number): number {
+      return (a * b) / gcd(a, b);
+    }
+
+    return numbers.reduce((acc, val) => lcm(acc, val), 1);
+  }
 }
 
 function parseInput(filename: string): Input {

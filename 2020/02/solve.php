f497c1914ee4068cd081691812232d7e33fc543b
Artyom Danilov <defytheflow@gmail.com>
2024-08-21 15:29:20 +0300

    refactor(2020/02): write in functional style

--- 2020/02/solve.php
+++ 2020/02/solve.php
@@ -1,10 +1,19 @@
 <?php
 
-main();
-
-function main()
+class ListEntry
 {
-    $input = parseInput(file_get_contents(__DIR__ . "/input.txt"));
+    public function __construct(
+        public int $minCount,
+        public int $maxCount,
+        public string $letter,
+        public string $password,
+    )
+    {}
+}
+
+function main(): void
+{
+    $input = parseInput("input.txt");
 
     $resultOne = solveOne($input);
     echo $resultOne, PHP_EOL;
@@ -18,67 +27,37 @@ function main()
 /** @param ListEntry[] $input */
 function solveOne(array $input): int
 {
-    return count(array_filter($input, function ($entry) {
-        return $entry->isValid();
-    }));
+    $isValid = function (ListEntry $entry) {
+        $letterCount = substr_count($entry->password, $entry->letter);
+        return $entry->minCount <= $letterCount && $letterCount <= $entry->maxCount;
+    };
+
+    return count(array_filter($input, $isValid));
 }
 
 /** @param ListEntry[] $input */
 function solveTwo(array $input): int
 {
-    return count(array_filter($input, function ($entry) {
-        return $entry->isValidNew();
-    }));
+    $isValid = function (ListEntry $entry) {
+        $i = $entry->minCount - 1;
+        $j = $entry->maxCount - 1;
+        return ($entry->password[$i] == $entry->letter) != ($entry->password[$j] == $entry->letter);
+    };
+
+    return count(array_filter($input, $isValid));
 }
 
 /** @return ListEntry[] */
-function parseInput(string $input)
+function parseInput(string $filename): array
 {
-    return array_map(
-        function ($line) {
-            return ListEntry::fromInput($line);
-        },
-        explode(PHP_EOL, trim($input))
-    );
-}
-
-class ListEntry
-{
-    public function __construct(
-        private int $minCount,
-        private int $maxCount,
-        private string $letter,
-        private string $password,
-    )
-    {
-    }
-
-    public function isValid(): bool
-    {
-        $letterCount = substr_count($this->password, $this->letter);
-        return $this->minCount <= $letterCount && $letterCount <= $this->maxCount;
-    }
-
-    public function isValidNew(): bool
-    {
-        $firstIndex = $this->minCount - 1;
-        $secondIndex = $this->maxCount - 1;
-
-        $firstCondition = $this->password[$firstIndex] == $this->letter;
-        $secondCondition = $this->password[$secondIndex] == $this->letter;
-
-        return $firstCondition != $secondCondition;
-    }
-
-    public static function fromInput(string $line): ListEntry
-    {
+    $parseListEntry = function (string $line) {
         [$policy, $letter, $password] = explode(" ", $line);
-        [$minCount, $maxCount] = explode("-", $policy);
+        [$minCount, $maxCount] = array_map("intval", explode("-", $policy));
+        return new ListEntry($minCount, $maxCount, rtrim($letter, ":"), $password);
+    };
 
-        $letter = rtrim($letter, ":");
-        $minCount = (int)$minCount;
-        $maxCount = (int)$maxCount;
-
-        return new static($minCount, $maxCount, $letter, $password);
-    }
+    $input = trim(file_get_contents(__DIR__ . "/" . $filename));
+    return array_map($parseListEntry, explode(PHP_EOL, $input));
 }
+
+main();

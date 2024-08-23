9356eb8f9b5742fbb1dc043c65831a9954730f50
Artyom Danilov <defytheflow@gmail.com>
2024-08-23 16:38:47 +0300

    feat(2020): solve second part of day 9

--- 2020/09/solve.php
+++ 2020/09/solve.php
@@ -7,6 +7,10 @@ function main(): void
     $resultOne = solveOne($input);
     echo $resultOne, PHP_EOL;
     assert($resultOne == 10_884_537);
+
+    $resultTwo = solveTwo($input);
+    echo $resultTwo, PHP_EOL;
+    assert($resultTwo == 1_261_309);
 }
 
 /** @param int[] $input */
@@ -25,6 +29,40 @@ function solveOne(array $input): int
     return -1;
 }
 
+/** @param int[] $input */
+function solveTwo(array $input): int
+{
+    $target = solveOne($input);
+
+    for ($i = 0; $i < count($input); $i++) {
+        $set = [$input[$i]];
+
+        for ($j = $i + 1; $j < count($input); $j++) {
+            $sum = array_sum($set);
+
+            if ($sum == $target) {
+                return min($set) + max($set);
+            }
+
+            if ($sum > $target) {
+                continue 2;
+            }
+
+            array_push($set, $input[$j]);
+        }
+
+    }
+
+    return -1;
+}
+
+/** @return int[] */
+function parseInput(string $filename): array
+{
+    $input = trim(file_get_contents(__DIR__ . "/" . $filename));
+    return array_map('intval', explode(PHP_EOL, $input));
+}
+
 /** @param int[] $numbers */
 function twoSum(array $numbers, int $target): bool
 {
@@ -39,13 +77,6 @@ function twoSum(array $numbers, int $target): bool
         }
     }
     return false;
-}
-
-/** @return int[] */
-function parseInput(string $filename): array
-{
-    $input = trim(file_get_contents(__DIR__ . "/" . $filename));
-    return array_map('intval', explode(PHP_EOL, $input));
 }
 
 main();

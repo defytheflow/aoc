b95541b0a5f037e667abfec38c748a95ca752b69
Artyom Danilov <defytheflow@gmail.com>
2024-08-27 11:32:06 +0300

    feat(2020): solve second part of day 11

--- 2020/11/solve.php
+++ 2020/11/solve.php
@@ -7,6 +7,10 @@ function main(): void
     $resultOne = solveOne($input);
     echo $resultOne, PHP_EOL;
     assert($resultOne == 2_283);
+
+    $resultTwo = solveTwo($input);
+    echo $resultTwo, PHP_EOL;
+    assert($resultTwo == 2_054);
 }
 
 /** @param PositionType[][] $input */
@@ -17,7 +21,7 @@ function solveOne(array $input): int
 
     while ($currentLayout != $previousLayout) {
         $previousLayout = $currentLayout;
-        $currentLayout = createNextLayout($currentLayout);
+        $currentLayout = createNextLayout($currentLayout, 'getAdjacentOccupiedSeats', 4);
     }
 
     $count = 0;
@@ -33,8 +37,52 @@ function solveOne(array $input): int
     return $count;
 }
 
+/** @param PositionType[][] $input */
+function solveTwo(array $input): int
+{
+    $previousLayout = null;
+    $currentLayout = &$input;
+
+    while ($currentLayout != $previousLayout) {
+        $previousLayout = $currentLayout;
+        $currentLayout = createNextLayout($currentLayout, 'getAdjacentOccupiedSeatsTwo', 5);
+    }
+
+    $count = 0;
+
+    foreach ($currentLayout as $row) {
+        foreach ($row as $seat) {
+            if ($seat == PositionType::OCCUPIED_SEAT) {
+                $count++;
+            }
+        }
+    }
+
+    return $count;
+}
+
+/** @return PositionType[][] */
+function parseInput(string $filename): array
+{
+    $input = trim(file_get_contents(__DIR__ . "/" . $filename));
+
+    return array_map(
+        function ($line) {
+            return array_map('PositionType::from', str_split($line));
+        },
+        explode(PHP_EOL, $input)
+    );
+}
+
+enum PositionType: string
+{
+    case FLOOR = ".";
+    case EMPTY_SEAT = "L";
+    case OCCUPIED_SEAT = "#";
+}
+
 /** @param PositionType[][] $layout */
-function createNextLayout(array &$layout): array
+function createNextLayout(array &$layout, callable $fn, int $limit): array
 {
     $nextLayout = [];
 
@@ -42,12 +90,12 @@ function createNextLayout(array &$layout): array
         $nextRow = [];
 
         for ($x = 0; $x < count($layout[$y]); $x++) {
-            $adjacent = getAdjacentOccupiedSeats($layout, $x, $y);
+            $adjacent = $fn($layout, $x, $y);
             $seat = $layout[$y][$x];
 
             if ($seat == PositionType::EMPTY_SEAT && $adjacent == 0) {
                 $seat = PositionType::OCCUPIED_SEAT;
-            } else if ($seat == PositionType::OCCUPIED_SEAT && $adjacent >= 4) {
+            } else if ($seat == PositionType::OCCUPIED_SEAT && $adjacent >= $limit) {
                 $seat = PositionType::EMPTY_SEAT;
             }
 
@@ -92,35 +140,45 @@ function getAdjacentOccupiedSeats(array &$layout, int $x, int $y): int
     return $count;
 }
 
-/** @return PositionType[][] */
-function parseInput(string $filename): array
+function getAdjacentOccupiedSeatsTwo(array &$layout, int $x, int $y): int
 {
-    $input = trim(file_get_contents(__DIR__ . "/" . $filename));
+    $count = 0;
+    $width = count($layout[$y]);
+    $height = count($layout);
+    $deltas = [
+        [-1, +0],
+        [+1, +0],
+        [+0, -1],
+        [+0, +1],
+        [-1, -1],
+        [-1, +1],
+        [+1, -1],
+        [+1, +1],
+    ];
 
-    return array_map(
-        function ($line) {
-            return array_map('PositionType::from', str_split($line));
-        },
-        explode(PHP_EOL, $input)
-    );
-}
+    foreach ($deltas as [$dx, $dy]) {
+        $ax = $x + $dx;
+        $ay = $y + $dy;
 
-enum PositionType: string
-{
-    case FLOOR = ".";
-    case EMPTY_SEAT = "L";
-    case OCCUPIED_SEAT = "#";
-}
-
-/** @param PositionType[][] $layout */
-function printLayout(array $layout): void
-{
-    foreach ($layout as $row) {
-        foreach ($row as $seat) {
-            echo $seat->value;
+        // move in that direciton until we meet a seat or reach the end
+        while (
+            (0 <= $ax && $ax < $width) &&
+            (0 <= $ay && $ay < $height)
+        ) {
+            $seat = $layout[$ay][$ax];
+            if ($seat == PositionType::OCCUPIED_SEAT) {
+                $count++;
+                continue 2;
+            } elseif ($seat == PositionType::EMPTY_SEAT) {
+                continue 2;
+            }
+            $ax += $dx;
+            $ay += $dy;
         }
-        echo "\n";
+
     }
+
+    return $count;
 }
 
 main();

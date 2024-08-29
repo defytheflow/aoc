f0a3223ad76aa14b45c02ea6bbef1d6a110490aa
Artyom Danilov <defytheflow@gmail.com>
2024-08-29 15:31:46 +0300

    feat(2020): solve second part of day 15

--- 2020/15/solve.php
+++ 2020/15/solve.php
@@ -9,48 +9,60 @@ function main(): void
     $resultOne = solveOne($input);
     echo $resultOne, PHP_EOL;
     assert($resultOne == 371);
+
+    $resultTwo = solveTwo($input);
+    echo $resultTwo, PHP_EOL;
+    assert($resultTwo == 352);
 }
 
 /** @param int[] $input */
 function solveOne(array $input): int
 {
-    /** @var int[] */
-    $spokenNumbers = $input;
-    $turn = count($input);
+    return solve($input, 2020);
+}
 
-    while ($turn <= 2020) {
-        $lastSpokenNum = $spokenNumbers[array_key_last($spokenNumbers)];
-        $isFirstTimeSpoken = array_count_values($spokenNumbers)[$lastSpokenNum] == 1;
+/** @param int[] $input */
+function solveTwo(array $input): int
+{
+    return solve($input, 30_000_000);
+}
+
+/** @param int[] $input */
+function solve(array $input, int $target): int
+{
+    /** @var array<int, int[]> */
+    $spokenNumbers = [];
+    foreach ($input as $i => $num) {
+        $spokenNumbers[$num] = [$i + 1];
+    }
+
+    $turn = count($input);
+    $lastSpokenNum = $input[count($input) - 1];
+
+    while ($turn < $target) {
+        $isFirstTimeSpoken = count($spokenNumbers[$lastSpokenNum]) == 1;
 
         if ($isFirstTimeSpoken) {
-            $spokenNumbers[] = 0;
+            $lastSpokenNum = 0;
         } else {
-            [$i, $j] = findLastTwo($spokenNumbers, $lastSpokenNum);
-            $spokenNumbers[] = $j - $i;
+            [$i, $j] = $spokenNumbers[$lastSpokenNum];
+            $lastSpokenNum = $j - $i;
         }
 
         $turn++;
-    }
 
-    return $lastSpokenNum;
-}
+        if (array_key_exists($lastSpokenNum, $spokenNumbers)) {
+            array_push($spokenNumbers[$lastSpokenNum], $turn);
 
-/**
- * @param int[] $numbers
- * @return int[]
- */
-function findLastTwo(array &$numbers, int $needle): array
-{
-    /** @var int[] */
-    $indices = [];
-
-    foreach ($numbers as $i => $num) {
-        if ($num == $needle) {
-            $indices[] = $i;
+            if (count($spokenNumbers[$lastSpokenNum]) > 2) {
+                array_shift($spokenNumbers[$lastSpokenNum]);
+            }
+        } else {
+            $spokenNumbers[$lastSpokenNum] = [$turn];
         }
     }
 
-    return array_slice($indices, -2);
+    return $lastSpokenNum;
 }
 
 /** @return int[] */

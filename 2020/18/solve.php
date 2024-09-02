91ec24c60a799faacceef15398ba1bd96782f6bf
Artyom Danilov <defytheflow@gmail.com>
2024-09-02 21:00:37 +0300

    refactor(2020/18): add `tokenize()`, use `array_reduce()`

--- 2020/18/solve.php
+++ 2020/18/solve.php
@@ -15,52 +15,42 @@ function main(): void
 /** @param string[] */
 function solveOne(array $input): int
 {
-    $total = 0;
-
-    foreach ($input as $line) {
-        $total += calculate($line);
-    }
-
-    return $total;
+    return array_reduce($input, fn ($total, $expr) => $total + calculate($expr), 0);
 }
 
-function calculate(string $expression): int
+function calculate(string $expr): int
 {
-    $openParen = strpos($expression, "(");
+    $openParen = strpos($expr, "(");
 
-    if (is_int($openParen)) {
-        $closeParen = findCloseParen($expression, $openParen);
-        $beforeParen = substr($expression, 0, $openParen);
-        $insideParen = substr($expression, $openParen + 1, $closeParen - $openParen - 1);
-        $afterParen = substr($expression, $closeParen + 1);
+    if ($openParen !== false) {
+        $closeParen = findCloseParen($expr, $openParen);
+        $beforeParen = substr($expr, 0, $openParen);
+        $insideParen = substr($expr, $openParen + 1, $closeParen - $openParen - 1);
+        $afterParen = substr($expr, $closeParen + 1);
         return calculate($beforeParen . calculate($insideParen) . $afterParen);
     }
 
+    /** @var int[] */
     $stack = [];
+    $tokens = tokenize($expr);
 
-    for ($i = 0; $i < strlen($expression); $i++) {
-        $char = $expression[$i];
+    for ($i = 0; $i < count($tokens); $i++) {
+        $token = $tokens[$i];
 
-        if (is_numeric($char)) {
-            $prevToken = $expression[$i - 2];
-            $buff = "";
-
-            while (is_numeric($char)) {
-                $buff .= $char;
-                $i++;
-                $char = $expression[$i];
-            }
-            $i--;
-
-            $value = intval($buff);
+        if (is_numeric($token)) {
+            $value = intval($token);
 
             if (count($stack) > 0) {
                 $prevValue = intval(array_pop($stack));
+                $prevToken = $tokens[$i - 1];
 
-                if ($prevToken == "+") {
-                    $value += $prevValue;
-                } elseif ($prevToken == "*") {
-                    $value *= $prevValue;
+                switch ($prevToken) {
+                    case "+":
+                        $value += $prevValue;
+                        break;
+                    case "*":
+                        $value *= $prevValue;
+                        break;
                 }
             }
 
@@ -71,23 +61,49 @@ function calculate(string $expression): int
     return $stack[0];
 }
 
-function findCloseParen(string $expression, int $openIndex): int
+function findCloseParen(string $expr, int $openIndex): int
 {
     $stack = 1;
     $i = $openIndex;
 
     while ($stack > 0) {
         $i++;
-        $char = $expression[$i];
 
-        if ($char == "(") {
-            $stack++;
-        } elseif ($char == ")") {
-            $stack--;
+        switch ($expr[$i]) {
+            case "(":
+                $stack++;
+                break;
+            case ")":
+                $stack--;
+                break;
         }
     }
 
     return $i;
+}
+
+/** @return string[] */
+function tokenize(string $expr): array
+{
+    /** @var string [] */
+    $tokens = [];
+
+    for ($i = 0; $i < strlen($expr); $i++) {
+        if ($expr[$i] == "+" || $expr[$i] == "*") {
+            array_push($tokens, $expr[$i]);
+        }
+        elseif (is_numeric($expr[$i])) {
+            $buffer = "";
+
+            for (; $i < strlen($expr) && is_numeric($expr[$i]); $i++) {
+                $buffer .= $expr[$i];
+            }
+
+            array_push($tokens, $buffer);
+        }
+    }
+
+    return $tokens;
 }
 
 /** @return string[] */
